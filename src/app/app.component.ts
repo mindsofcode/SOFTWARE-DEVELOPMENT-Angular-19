9dbee902a6eb006021ee282f447475559712bcac
B <official.moughazi@gmail.com>
2024-12-17 16:22:08 +0100

    Angular-19 | Signals Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
   title = 'hello-world';
+
+
+  quantity = signal(1);
+  unitPrice = signal(12);
+
+  totalPrice = computed(() => this.quantity() * this.unitPrice());
+
+  changeQuantity(amount: number) {
+    this.quantity.update(qty => qty + amount);
+  }
+
+  changePrice(amount: number) {
+    this.unitPrice.update(price => price + amount);
+  }
 }
